94a231a9eeb1f3d75764d3144f0b33709c1ea128
Mykola Kolisnyk <kolesnikny@gmail.com>
2022-01-03 16:08:08 +0200

    Add  task 4

--- index.js
+++ index.js
@@ -6,30 +6,82 @@
 const inputHeight = Number(prompt('Type height of the rectangle'));
 const inputWidth = Number(prompt('Type width of the rectangle'));
 
-function getSquare(height, width) {
+function getRectangleSquare(height, width) {
   return height * width;
 }
 
-if (isNaN(inputHeight) || isNaN(inputWidth5)) {
+if (isNaN(inputHeight) || isNaN(inputWidth)) {
   alert('The entered values do not match the conditions.');
 } else {
-  alert(`Rectangle area = ${getSquare(inputHeight, inputWidth)}`);
+  alert(`Rectangle area = ${getRectangleSquare(inputHeight, inputWidth)}`);
 }
 
 */
 
 //Task 2.
 /*Написать функцию, которая принимает диаметр и возвращает длину окружности.*/
+/*
+const PI_NUMBER = 3.14;
 
-const inputDiameter = Number(prompt('Type height of the rectangle'));
+const inputDiameter = Number(prompt('Type diameter'));
 
+function getCircumference(diameter) {
+  return diameter * PI_NUMBER;
+}
+
+if (isNaN(inputDiameter)) {
+  alert('The entered values do not match the conditions.');
+} else {
+  alert(`Circumference = ${getCircumference(inputDiameter)}`);
+}
+*/
 //Task 3.
-/*Написать функцию, которая принимает высоту и диаметру основания 
+/*Написать функцию, которая принимает высоту и диаметр у основания 
 циллиндра и возвращает его объем.*/
 
+/*
+const PI_NUMBER = 3.14;
+
+const inputHeight = Number(prompt('Type height of the cylinder'));
+const inputDiameter = Number(prompt('Type diametr of the cylinder'));
+
+function getCylinderVolume(height, diameter) {
+  return PI_NUMBER * ((diameter * diameter) / 4) * height;
+}
+
+if (isNaN(inputHeight) || isNaN(inputDiameter)) {
+  alert('The entered values do not match the conditions.');
+} else {
+  alert(`Cylinder Volume = ${getCylinderVolume(inputHeight, inputDiameter)}`);
+}
+*/
 //Task 4.
 /*Реализовать конвертер доллара (доллар-грн и грн-доллар, в зависимости от 
 переданного модификатора). Курс текущий на момент решения задачи*/
+
+const PriceDollar = 27.5;
+
+const inputModifier = Number(
+  prompt('Check convert modifier:\n0 - UAH to USD\n1 - USD to UAH')
+);
+const inputValue = Number(prompt('Enter the amount you want to convert:'));
+
+function converterMoney(modifier, value) {
+  switch (modifier) {
+    case 0:
+      return value / PriceDollar;
+    case 1:
+      return value * PriceDollar;
+    default:
+      return 'Not undesttand';
+  }
+}
+
+if (isNaN(inputValue)) {
+  alert('The entered values do not match the conditions.');
+} else {
+  alert(converterMoney(inputModifier, inputValue));
+}
 
 //Task 5.
 /*Выведите все числа в заданном диапазоне, которые делятся на 5. 
